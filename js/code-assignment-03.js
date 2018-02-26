f04cd090048722e4d160b1022460f9ab7311bfc8
jbekka1 <35709781+jbekka1@users.noreply.github.com>
2018-02-26 08:32:57 -0600

    2234

--- js/code-assignment-03.js
+++ js/code-assignment-03.js
@@ -3,3 +3,15 @@ let projectTitle = "Map 1"
 let projecturl = true
 let hasThumbnail = true
 let projectkeywords = ["web gis"]
+let myNumber = 7
+let myString = 'Some text'
+let myBoolean = true
+let myArray = ['apple', 'orange', 'pear']
+console.log(myNumber)
+console.log(myString)
+console.log(myBoolean)
+console.log(myArray)
+console.log('Project ID: ' + projectId)
+console.log('Title: ' + projectTitle)
+console.log('Does it have a thumbnail image? ' + hasThumbnail)
+console.log('Keywords: ' + projectKeywords)
