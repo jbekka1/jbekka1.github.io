175dc59bde2005aaaaafd4c599c75dc2b48a0f8d
jbekka1 <35709781+jbekka1@users.noreply.github.com>
2018-02-26 08:52:59 -0600

    235435

--- js/code-assignment-03.js
+++ js/code-assignment-03.js
@@ -1,6 +1,7 @@
 let projectId = 1
 let projectTitle = 'Book Assignment 1'
 let projectUrl = 'http://arcg.is/2DHACvD'
+let projectDesc = 'An online map tour of National Parks in Utah.'
 let hasThumbnail = true
 let projectKeywords = ["web gis"]
 let myNumber = 7
@@ -11,7 +12,8 @@ console.log(myNumber)
 console.log(myString)
 console.log(myBoolean)
 console.log(myArray)
-console.log('Project ID: ' + projectId)
+console.log('ID: ' + projectId)
 console.log('Title: ' + projectTitle)
+console.log('URL: ' + projectUrl)
 console.log('Does it have a thumbnail image? ' + hasThumbnail)
 console.log('Keywords: ' + projectKeywords)
