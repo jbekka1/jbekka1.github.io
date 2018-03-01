e875c6d1fb0d38a92f967465a15b80156479d9a1
jbekka1 <35709781+jbekka1@users.noreply.github.com>
2018-03-01 11:18:52 -0600

    342

--- js/code-assignment-04.js
+++ js/code-assignment-04.js
@@ -1,45 +1,25 @@
-// This is my array
+// This is my array within an array
 let geogArray = [
   24,
   'http://arcg.is/2DHACvD',
   'Book Assignment 1',
   'An online map tour of National Parks in Utah.',
   true
-]
-
-// This is my object. Each value has a corresponding property.
-let geogObject = {
-  id: 24,
-  url: 'http://arcg.is/2DHACvD',
-  title: 'Book Assignment 1',
-  desc: 'An online map tour of National Parks in Utah.',
-  thumb: true,
-}
-
-// My array within an array (array-ception?)
-let geogArray = [
-  24,
-  'http://arcg.is/2DHACvD',
-  'Book Assignment 1',
-  'An online map tour of National Parks in Utah.',
-  true,
   ['code04', 'webgis', 'jbekka1']
 ]
-
-// This is my object within an object (object-ception?)
+// This is my object within an object. Each value has a corresponding property.
 let geogObject = {
   id: 24,
   url: 'http://arcg.is/2DHACvD',
   title: 'Book Assignment 1',
   desc: 'An online map tour of National Parks in Utah.',
   thumb: true,
-  keywords: [
+  keyords: [
     'code04',
     'webgis',
     'jbekka1',
   ]
 }
-
 console.log(geogArray[3]);
 console.log(geogObject.desc);
 console.log(geogObject['desc']);
