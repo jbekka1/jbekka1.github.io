34ec97d749e99474722e35af09d014d8ec5cad0e
jbekka1 <35709781+jbekka1@users.noreply.github.com>
2018-03-12 15:29:12 -0500

    345678

--- js/code-assignment-05.js
+++ js/code-assignment-05.js
@@ -1,14 +1,21 @@
-let webgisProject3 = {
-  id: 3
+let project3 = {
+  id: 3,
   desc: "An online form allowing people to publish possible suspects in a local area.",
   hasScreenshot: true
 }
-let webgisProject5 = {
-  id: 5
+let project4 = {
+  id: 5,
   desc: "A data viewer showing US populaton growth through different features and widgets.",
   hasScreenshot: true
 }
-let webgisProjects = [
-  webgisProject3,
-  webgisProject4,
+let projects = [
+  project3,
+  project4
 ]
+for (let projectIndex = 0; projectIndex < projects.length; projectIndex++) {
+  console.log('Element ' + projectIndex + 'desc: ' + projects[projectIndex].desc);
+  console.log('images/ss-project-' + projects[projectIndex].id + '.png');
+}
+if (projects[projectIndex].hasScreenshot === true) {
+  console.log('images/ss-project-' + projects[projectIndex].id + '.png')
+}
