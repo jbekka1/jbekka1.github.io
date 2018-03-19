68df380532aedcc2c36e6d7b0b7f4cb84bf5b1f3
jbekka1 <35709781+jbekka1@users.noreply.github.com>
2018-03-19 17:13:55 -0500

    xczvx

--- map/webmap-assignment-01.js
+++ map/webmap-assignment-01.js
@@ -1,4 +1,5 @@
 //let someName = L.map('your-map-div-id').setView([yourLat, yourLon], yourZoom)
 let mymap = L.map('jasminesmapid').setView([51.505, -0.09], 15)
 let basemapUrl = 'https://cartodb-basemaps-{s}.global.ssl.fastly.net/dark_nolabels/{z}/{x}/{y}.png'
-L.titleLayer(basemapUrl).addTo(mymap)
+let layer = L.titleLayer(basemapUrl).addTo(mymap)
+let marker = L.marker([51.5, -0.09]).addTo(mymap)
