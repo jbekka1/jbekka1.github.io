78746215744af2258fa6e9e1792429e8f863787e
jbekka1 <35709781+jbekka1@users.noreply.github.com>
2018-03-19 13:17:12 -0500

    45345

--- js/code-assignment-05.js
+++ js/code-assignment-05.js
@@ -1,11 +1,11 @@
 let projectData = []
 
-let projectData[0] = {
+projectData[0] = {
   id: 3,
   desc: "An online form allowing people to publish possible suspects in a local area.",
   hasScreenshot: true
 }
-let projectData[1] = {
+projectData[1] = {
   id: 4,
   desc: "A data viewer showing US populaton growth through different features and widgets.",
   hasScreenshot: true
