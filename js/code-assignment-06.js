fe41c6ceef2249ac232916caa2f8522199fd30f7
jbekka1 <35709781+jbekka1@users.noreply.github.com>
2018-03-19 13:33:05 -0500

    435345

--- js/code-assignment-06.js
+++ js/code-assignment-06.js
@@ -1,8 +1,32 @@
-const describeFlower =
-  (flower) => {
-  let description = 'The ' + flower + 'has a beautiful red hue. '
-  return description
+let projectData = []
+
+projectData[0] = {
+  id: 3,
+  desc: "An online form allowing people to publish possible suspects in a local area.",
+  hasScreenshot: true
 }
-let myFavoriteFlower = 'poppy'
-let description = decribeFlower(myFavoriteFlower)
-console.log(description)
+projectData[1] = {
+  id: 4,
+  desc: "A data viewer showing US populaton growth through different features and widgets.",
+  hasScreenshot: true
+}
+
+function describeScreenshot (project) {
+  let describeScreenshot = 'Project ' + project.id + ' - ' + project.desc
+  return describeScreenshot
+}
+
+function describeScreenshotSrc (project) {
+  if (project.hasScreenshot === true) {
+    let describeScreenshotSrc = 'images/ss-project-' + project.id + '.png'
+    return descibeScreenshotSrc
+  }
+  return 'images/no-preview.png'
+}
+
+for (let i = 0; i < projectData.length; i++) {
+  let screenshotDescription = describeScreenshot(projectData[i])
+  let screenshotSrc = describeScreenshotSrc(projectData[i])
+  console.log(screenshotDescription)
+  console.log(screenshotSrc)
+}
