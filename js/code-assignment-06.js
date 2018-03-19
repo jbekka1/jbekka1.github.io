81c80487471f8a22633bb0e5f9f96ad3dbc94ddf
jbekka1 <35709781+jbekka1@users.noreply.github.com>
2018-03-19 12:50:24 -0500

    34

--- js/code-assignment-06.js
+++ js/code-assignment-06.js
@@ -1,3 +1,7 @@
 function describeFlower (flower) {
   let description = 'The ' + flower + 'has a beautiful red hue. '
+  return description
 }
+let myFavoriteFlower = 'poppy'
+let description = decribeFlower(myFavoriteFlower)
+console.log(description)
