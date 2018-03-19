12eac228b2fec91f2e3bfb1d5604311647bf4155
jbekka1 <35709781+jbekka1@users.noreply.github.com>
2018-03-19 17:16:30 -0500

    zxzx

--- map/webmap-assignment-01.js
+++ map/webmap-assignment-01.js
@@ -1,5 +1,10 @@
 //let someName = L.map('your-map-div-id').setView([yourLat, yourLon], yourZoom)
 let mymap = L.map('jasminesmapid').setView([51.505, -0.09], 15)
-let basemapUrl = 'https://map1.vis.earthdata.nasa.gov/wmts-webmerc/VIIRS_CityLights_2012/default/{time}/{tilematrixset}{maxZoom}/{z}/{y}/{x}.{format}'
+let basemapUrl = 'https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png'
 L.tileLayer(basemapUrl).addTo(mymap)
 let marker = L.marker([51.5, -0.09]).addTo(mymap)
+let polygon = L.polygon([
+  [51.509, -0.08],
+  [51.503, -0.06],
+  [51.51, -0.047]
+]).addTo(mymap);
