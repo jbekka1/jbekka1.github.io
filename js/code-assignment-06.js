a3309555e367cdc7ab2d0ac22c4b3901bcf20ea6
jbekka1 <35709781+jbekka1@users.noreply.github.com>
2018-03-19 12:44:56 -0500

    3423

--- js/code-assignment-06.js
+++ js/code-assignment-06.js
@@ -1,5 +1,6 @@
-function describeFlower (flower)
-{let description = 'The ' + flower + 'is a beautiful red hue.'}
+const describeFlower = (flower) => {
+  let description = 'The ' + flower + 'has a beautiful red hue. '
+}
 return (description)
 let myFavoriteFlower = 'poppy'
 let description = describeFlower(myFavoriteFlower)
