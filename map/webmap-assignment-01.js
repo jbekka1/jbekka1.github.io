ecb60a268898f0781e119542dfd04d1f16edb5aa
jbekka1 <35709781+jbekka1@users.noreply.github.com>
2018-03-19 17:11:02 -0500

    zx

--- map/webmap-assignment-01.js
+++ map/webmap-assignment-01.js
@@ -1,4 +1,4 @@
 //let someName = L.map('your-map-div-id').setView([yourLat, yourLon], yourZoom)
 let mymap = L.map('jasminesmapid').setView([51.505, -0.09], 15)
-let basemapUrl = 'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}'
+let basemapUrl = 'https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png'
 L.titleLayer(basemapUrl).addTo(mymap)
