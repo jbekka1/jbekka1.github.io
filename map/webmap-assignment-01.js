52a91469abd302eff40c899ad983f0eb198044d4
jbekka1 <35709781+jbekka1@users.noreply.github.com>
2018-03-19 17:11:43 -0500

    sdf

--- map/webmap-assignment-01.js
+++ map/webmap-assignment-01.js
@@ -1,4 +1,4 @@
 //let someName = L.map('your-map-div-id').setView([yourLat, yourLon], yourZoom)
 let mymap = L.map('jasminesmapid').setView([51.505, -0.09], 15)
-let basemapUrl = 'https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png'
+let basemapUrl = 'https://cartodb-basemaps-{s}.global.ssl.fastly.net/dark_nolabels/{z}/{x}/{y}.png'
 L.titleLayer(basemapUrl).addTo(mymap)
