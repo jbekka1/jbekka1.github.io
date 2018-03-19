dad75fe9ed5f2c7dba9104adbc92dd43acc4fb35
jbekka1 <35709781+jbekka1@users.noreply.github.com>
2018-03-19 13:16:15 -0500

    ersr

--- js/code-assignment-05.js
+++ js/code-assignment-05.js
@@ -1,23 +1,20 @@
 let projectData = []
 
-let project3 = {
+let projectData[0] = {
   id: 3,
   desc: "An online form allowing people to publish possible suspects in a local area.",
   hasScreenshot: true
 }
-let project4 = {
-  id: 5,
+let projectData[1] = {
+  id: 4,
   desc: "A data viewer showing US populaton growth through different features and widgets.",
   hasScreenshot: true
 }
-let projects = [
-  project3,
-  project4
-]
-for (let i = 0; i < projects.length; i++) {
-  console.log('Element ' + i + 'desc: ' + projects[i].desc);
-  console.log('images/ss-project-' + projects[i].id + '.png');
+
+for (let i = 0; i < projectData.length; i++) {
+  console.log('Index position ' + i + 'desc: ' + projectData[i].desc);
+  if (projectData[i].hasScreenshot === true) {
+    console.log('images/ss-project-' + projectData[i].id + '.png')
+} else {
+  console.log(images/no-preview.png)
 }
-if (projects[i].hasScreenshot === true) {
-  console.log('images/ss-project-' + projects[i].id + '.png');
-} // need help understanding why my conditional statement is not being logged
