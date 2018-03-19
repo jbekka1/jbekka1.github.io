bc88e5fc4d2f8f9b3b8707dc126e8eaf8aaad64f
jbekka1 <35709781+jbekka1@users.noreply.github.com>
2018-03-19 17:09:13 -0500

    dsad

--- map/webmap-assignment-01.js
+++ map/webmap-assignment-01.js
@@ -1,4 +1,4 @@
 //let someName = L.map('your-map-div-id').setView([yourLat, yourLon], yourZoom)
 let mymap = L.map('jasminesmapid').setView([51.505, -0.09], 15)
-let basemapUrl = 'https://map1.vis.earthdata.nasa.gov/wmts-webmerc/MODIS_Terra_CorrectedReflectance_Bands367/default/{time}/{tilematrixset}{maxZoom}/{z}/{y}/{x}.{format}'
-L.tileLayer(basemapUrl).addTo(mymap)
+let basemapUrl = 'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}'
+L.titleLayer(basemapUrl).addTo(mymap)
