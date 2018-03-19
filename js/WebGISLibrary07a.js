0ad7e473213704e4a3586c3d6a21b083310693a2
jbekka1 <35709781+jbekka1@users.noreply.github.com>
2018-03-19 14:40:48 -0500

    ffdsf

--- js/WebGISLibrary07a.js
+++ js/WebGISLibrary07a.js
@@ -1,15 +1,3 @@
-projectData[0] = {
-  id: 3,
-  desc: "An online form allowing people to publish possible suspects in a local area.",
-  hasScreenshot: true
-  }
-
-projectData[1] = {
-  id: 5,
-  desc: "A data viewer showing US populaton growth through different features and widgets.",
-  hasScreenshot: false
-}
-
 function describeScreenshot (project) {
   let describeScreenshot = 'Project ' + project.id + ' - ' + project.desc
   return describeScreenshot
@@ -22,11 +10,4 @@ function describeScreenshotSrc (project) {
   } else {
     return 'no-preview.png'
   }
-}
-
-for (let i = 0; i < projectData.length; i++) {
-  let screenshotDescription = describeScreenshot(projectData[i])
-  let screenshotSrc = describeScreenshotSrc(projectData[i])
-  console.log(screenshotDescription)
-  console.log(screenshotSrc)
 }
