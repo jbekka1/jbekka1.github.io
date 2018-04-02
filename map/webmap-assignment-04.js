a51ca78166a042fd83676624785fe1842456d0de
jbekka1 <35709781+jbekka1@users.noreply.github.com>
2018-04-02 18:17:51 -0500

    sasa

--- map/webmap-assignment-04.js
+++ map/webmap-assignment-04.js
@@ -3,9 +3,20 @@ let colorado = L.map('jasminesmapid').setView([39.11748, -105.7435], 4)
 let basemapUrl ="https://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}"
 L.tileLayer(basemapUrl).addTo(colorado)
 
-let stateStyle = {
-  color: "purple"
+let stateStyle = function () {
+  let formatting = {
+    color: "purple",
+    weight: 3,
+    fillOpacity: 0.4
+  }
+  return formatting
 }
+
+function stateStyle (feature) {
+  let age =
+  let stateColor =
+}
+
 let stateOptions = {
   style: stateStyle
 }
