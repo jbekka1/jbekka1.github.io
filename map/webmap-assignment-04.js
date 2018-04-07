d87f65503f3b029e6895407deb73acd9c56da312
jbekka1 <35709781+jbekka1@users.noreply.github.com>
2018-04-07 13:44:57 -0500

    453453

--- map/webmap-assignment-04.js
+++ map/webmap-assignment-04.js
@@ -1,7 +1,7 @@
 //let someName = L.map('your-map-div-id').setView([yourLat, yourLon], yourZoom)
-let colorado = L.map('jasminesmapid').setView([39.11748, -105.7435], 4)
+let coloradoMap = L.map('jasminesmapid').setView([39.11748, -105.7435], 7)
 let basemapUrl ="https://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}"
-L.tileLayer(basemapUrl).addTo(colorado)
+L.tileLayer(basemapUrl).addTo(coloradoMap)
 
 let stateStyle = function () {
   let formatting = {
@@ -11,13 +11,7 @@ let stateStyle = function () {
   }
   return formatting
 }
-
-function stateStyle (feature) {
-  let age =
-  let stateColor =
-}
-
 let stateOptions = {
   style: stateStyle
 }
-L.geoJSON(stateDemographics, stateOptions).addTo(colorado)
+L.geoJSON(coloradoVegetation, stateOptions).addTo(coloradoMap)
