54765a127137c5c5a0b13874216077e0cce87bbc
jbekka1 <35709781+jbekka1@users.noreply.github.com>
2018-04-08 21:58:28 -0500

    dsadsad

--- map/webmap-assignment-04.js
+++ map/webmap-assignment-04.js
@@ -1,17 +1,28 @@
 //let someName = L.map('your-map-div-id').setView([yourLat, yourLon], yourZoom)
-let coloradoMap = L.map('jasminesmapid').setView([39.951121, -105.294346], 16)
+let coloradoMap = L.map('jasminesmapid').setView([40.015769, -105.29263], 11)
 let basemapUrl ="https://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}"
+coloradoMap.on('click', function (event){
+  console.log('You clicked the map at ' + event.latlng)
+})
 L.tileLayer(basemapUrl).addTo(coloradoMap)
-
-let stateStyle = function () {
-  let formatting = {
-    color: "purple",
-    weight: 3,
+function wildlifeClosureStyle (feature) {
+  let closureType = feature.properties.Species
+  let locationColor = 'Violet'
+  if ( closureType = ' Bat ') {locationColor = 'Purple'}
+  let wildlifeClosureStyle = {
+    color: locationColor,
+    weight: 2,
     fillOpacity: 0.4
   }
-  return formatting
+  return wildlifeClosureStyle
 }
-let stateOptions = {
-  style: stateStyle
+let wildlifeClosureOptions = {
+  style: wildlifeClosureStyle,
+  onEachFeature: wildlifeClosurePopup
 }
-L.geoJSON(coloradoRockClimbing, stateOptions).addTo(coloradoMap)
+function wildlifeClosurePopup (feature, layer) {
+  let closureTypeName = feature.properties.ClosureNam
+  let closureType = feature.properties.Species
+  layer.bindPopup('The area of ' + closureTypeName + ' has a seasonal wildlife closure due to a species of ' + closureType)
+}
+L.geoJSON(boulderWildlifeClosures, wildlifeClosureOptions).addTo(coloradoMap)
