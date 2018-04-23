907cc1140d34d9439b1396bd0d6c70d14d52caab
jbekka1 <35709781+jbekka1@users.noreply.github.com>
2018-04-22 22:04:58 -0500

    jnkj

--- canada-summer-roadtrip/roadtrip-webmap.js
+++ canada-summer-roadtrip/roadtrip-webmap.js
@@ -1,3 +1,17 @@
-let summerRoadtrip = L.map('roadtripmapid').setView([51.505, -0.09], 13)
-let basemapUrl = 'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}'
-L.tileLayer(basemapUrl).addTo(summerRoadtrip)
+let summerRoadtrip = L.map("roadtripmapid").setView([41.089, -105.504], 4)
+summerRoadtrip.on('click', function (event){
+  console.log('You clicked the map at ' + event.latlng)
+})
+
+let satelliteBasemap = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}')
+let topoBasemap = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}').addTo(summerRoadtrip)
+let streetBasemap = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}')
+
+let roadtripBasemaps = {
+  'Street Map': streetBasemap,
+  'Satellite Map': satelliteBasemap,
+  'Topographic Map': topoBasemap,
+}
+L.control.layers(roadtripBasemaps).addTo(summerRoadtrip)
+
+let carsonNationalForest = L.marker([36.521275, -106.190435]).addTo(summerRoadtrip)
